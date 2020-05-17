61247da6510cf7cd49a1c2f7ba3dde9b0c85f7fb
Joanna Wu <boxbox557@gmail.com>
2020-05-17 19:00:31 +0800

    Add AffinityPropagation alg support

--- cluster.py
+++ cluster.py
@@ -1,8 +1,7 @@
 import json
 import numpy as np
-# from sklearn.cluster import AffinityPropagation
 
-def get_input(news_path, embed_path):
+def get_input(news_path, embed_path, debugging=False):
 
     with open(news_path, 'r', encoding='utf-8') as f:
         lines = f.read().split('\n')[:-1]    # abandon empty string
@@ -17,30 +16,54 @@ def get_input(news_path, embed_path):
     with open(embed_path, 'r', encoding='utf-8') as f:
         lines = f.read().split('\n')[:-1]    # abandon empty string
     
+    if debugging:
+        debug = open('output/temp.debug', 'w+', encoding='utf-8')
     features = []
     for idx, line in enumerate(lines):
         data = json.loads(line)
         word_embedding = []
+        sent = ''
         for word in data['features']:
             if word['token'] == '[CLS]':
                 for layer in word['layers']:
                     word_embedding.append(layer['values'])
+            elif word['token'] != '[SEP]':
+                sent += word['token']
         word_embedding = np.sum(word_embedding, axis=0)
         features.append(word_embedding)
         news_dict[idx]['embedding'] = word_embedding.tolist()
+
+        if debugging:
+            debug.write(sent+'\n')
+            debug.write(news_dict[idx]['title']+'\t'+news_dict[idx]['content']+'\n')
+            debug.write(str(word_embedding.tolist())+'\n')
+    
     features = np.vstack([news['embedding'] for news in news_dict.values()])
+    if debugging:
+        debug.close()
 
     return news_dict, features
 
-def cluster_news(news, features, n):
+def cluster_news(news, features, alg, n=None):
     print('[ INFO ] start clustering...')
+    if alg.lower() == 'kmean':
+        from sklearn.cluster import KMeans
+        kmean = KMeans(n)
+        kmean.fit(features)
+        labels = kmean.labels_
+        n_cluster = n
+    elif alg.lower() in ['affinitypropagation', 'ap']:
+        from sklearn.cluster import AffinityPropagation
+        ap = AffinityPropagation(random_state=None)
+        ap.fit(features)
+        labels = ap.labels_
+        n_cluster = max(labels) + 1
+    else :
+        print('[ ERROR ] Algorithm not supported.')
+        return [[]]
 
-    from sklearn.cluster import KMeans
-    kmean = KMeans(n)
-    kmean.fit(features)
-
-    clusters = [[] for i in range(n)]
-    for idx, cluster_id in enumerate(kmean.labels_):
+    clusters = [[] for i in range(n_cluster)]
+    for idx, cluster_id in enumerate(labels):
         clusters[cluster_id].append(news[idx])
     return clusters
 
@@ -60,32 +83,10 @@ if __name__=='__main__':
     parser.add_argument('embedding', type=str, help='BERT output embedding file')
     parser.add_argument('output', type=str, default='result.json', help='clustering output file')
     parser.add_argument('-alg', '--algorithm', type=str, default='KMean', help='Clustering algorithm')
-    parser.add_argument('-n', type=int, default=5, help='clustering groupt count')
+    parser.add_argument('-n', type=int, default=5, help='clustering groupt count (available for KMean)')
+    parser.add_argument('-d', '--debug', action='store_true', help='debug mode')
     args = parser.parse_args()
 
-    news, features = get_input(args.news, args.embedding)
-    clusters = cluster_news(news, features, args.n)
+    news, features = get_input(args.news, args.embedding, debugging=args.debug)
+    clusters = cluster_news(news, features, args.algorithm, n=args.n)
     dump_json = dump_json(clusters, args.output)
-
-# clusters_arr = []
-# for i, cluster in enumerate(clusters):
-#     news_info = []
-#     for sent_id, emb in cluster:
-#         info = {}
-#         info['title'] = news[sent_id][0]
-#         info['content'] = news[sent_id][1]
-#         info['embedding'] = emb.tolist()
-
-#         news_info.append(info)
-#     clusters_arr.append(news_info)
-# d = {}
-# d['result'] = clusters_arr
-
-# outputfile = 'output/cluster_title.txt'
-
-        # output.write("Cluster {}\n".format(i+1))
-        # for sentence in cluster:
-        #     output.write('\t'+sentence+'\n')
-        # output.write("-------\n\n")
-
-# clustering = AffinityPropagation(random_state=5).fit(features)
